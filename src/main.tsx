0ad81d8aed3623e4ca8f063249eb02c4d24825d6
Yur1kkkj <anabelduarteroblox@gmail.com>
2023-07-01 15:02:20 -0400

    criação do projeto react com Vite

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { Teste } from './Teste'
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Teste />
+  </React.StrictMode>,
+)
